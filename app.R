c9bc859794b4710df5c210629bb53d618cb39876
Leventcan Gültekin <leventcan.gultekin@gmail.com>
2019-02-17 20:02:59 +0300

    Sidebar'da eklemeler yapıldı.

--- app.R
+++ app.R
@@ -1,12 +1,17 @@
 library(shiny)
 library(readxl)
-library(sf)
+library(tibble)
 library(ggplot2)
 library(dplyr)
+library(RColorBrewer)
+library(sf)
 
 
-tr2_map <- st_read("maps/TR2_Map.json")
-tr3_map <- st_read("maps/TR3_Map.json")
+options(shiny.sanitize.errors=FALSE)
+
+tr2_map <- st_read("https://raw.githubusercontent.com/Leventcan/spatial_files/master/TR2_Map.json")
+tr3_map <- st_read("https://raw.githubusercontent.com/Leventcan/spatial_files/master/TR3_Map.json")
+nc <- st_read(system.file("shape/nc.shp", package="sf"))
 
 map_theme1 <- function(){
     theme_minimal() +
@@ -18,10 +23,13 @@ map_theme1 <- function(){
               legend.direction="vertical",
               plot.caption = element_text(size=5),
               plot.title = element_text(size=17),
-              legend.title=element_blank()
+              legend.title=element_blank(),
+              legend.spacing.x = unit(0.1, 'cm')
         ) 
 }
 
+pal_info <-brewer.pal.info
+pal_info <- rownames_to_column(pal_info, "palette")
 
 # Define UI for data upload app ----
 ui <- fluidPage(
@@ -36,22 +44,46 @@ ui <- fluidPage(
         sidebarPanel(
             
             # Input: Select a file ----
-            fileInput("file1", "Choose CSV File",
+            fileInput("file1", "Excel Dosyası Yükleyin",
                       multiple = TRUE,
                       accept = c("text/csv",
                                  "text/comma-separated-values,text/plain",
-                                 ".csv")),
+                                 ".csv",
+                                 ".xls",
+                                 ".xlsx")),
             
-            # Horizontal line ----
-            tags$hr()
+            selectInput("olcek", label = h4("Ölçek Seçiniz"), 
+                        choices = list("Düzey-2", "Düzey-3"), 
+                        selected = "Düzey-3"),
+            
+            selectInput("veri_tip", label = h4("Verinizin Tipini Seçiniz"),
+                        choices = list("Ardışık" = "seq", "Kategorik" = "qual",
+                                       "Ayrıksı" = "div"),
+                        selected = "seq"),
+            
+            uiOutput("map_palette"),
+            
+            numericInput("legend_col", label = h4("Lejand Kolon Sayısı"), value = 1),
+            
+            checkboxInput("etiket", label = "Etiket Olsun", value = FALSE),
+            
+            tags$h5("Yükleyeceğiniz excel dosyalarında ", 
+                    tags$a(href = "https://github.com/Leventcan/tr_mapping_app/raw/master/sample_excel_tr2.xlsx", "Düzey-2"),
+                    " ve ",
+                    tags$a(href = "https://github.com/Leventcan/tr_mapping_app/raw/master/sample_excel_tr3.xlsx", "Düzey-3"),
+                    " verileri için linkte belirtilen excel dosyalarının kullanılmasında fayda bulunmaktadır."),
+            
+            actionButton("map_button", "Haritayı Oluştur"),
+            
+            tags$h4("İndirme"),
+            
+            downloadButton('download', label = "İndir")
             
         ),
         
-        # Main panel for displaying outputs ----
+        
         mainPanel(
             
-            # Output: Data file ----
-            tableOutput("contents"),
             plotOutput("map01")
             
         )
@@ -62,28 +94,59 @@ ui <- fluidPage(
 # Define server logic to read selected file ----
 server <- function(input, output) {
     
-    output$contents <- renderTable({
+
+    df_data <- reactive({
         
-        # input$file1 will be NULL initially. After the user selects
-        # and uploads a file, head of that data file by default,
-        # or all rows if selected, will be shown.
+        validate(
+            need(input$file1$datapath != "", "Lütfen öncelikle excel dosyasını yükleyiniz...")
+        )
         
-        req(input$file1)
+        read_excel(input$file1$datapath) 
         
-        df <- read_excel(input$file1$datapath)
+    })
+    
+    map_data <- reactive(
         
-        return(df)
-        
-        
-             
+        if (input$olcek == "Düzey-3") {
+            tr3_map %>% left_join(df_data(), by = c("NUTS_ID" = "NUTS_ID")) %>% 
+                mutate(DATA =as.factor(DATA)) %>% rename(LABEL = IL_ADI)
+        } else {
+            tr2_map %>% left_join(df_data(), by = c("NUTS_ID" = "NUTS_ID")) %>% 
+                mutate(DATA =as.factor(DATA)) %>% mutate(LABEL = NUTS_ID)
+        }
+    )
+    
+
+    map_data1 <- eventReactive(input$map_button, {
+            
+        map_data()
+            
         })
+    
+    output$map_palette <- renderUI({
+        
+        chosen_pal <- filter(pal_info, category == input$veri_tip)$palette 
+        selectInput("palette", label = h4("Palet Seçiniz"),
+                    choices = chosen_pal)
+    })
     
     
     output$map01 <- renderPlot({
+        p <- ggplot(map_data1()) + geom_sf(aes(fill = DATA)) + 
+            map_theme1() + scale_fill_brewer(palette = input$palette) +
+            guides(fill=guide_legend(ncol=input$legend_col))
         
-        ggplot(tr3_map) + geom_sf() + map_theme1()
-        
-        
+        if (input$etiket == TRUE){
+            p + geom_sf_text( aes(label = LABEL), size = 2.3)
+        } else {
+            p
+        }
+            
+       
+    })
+    
+    output$deneme <- renderTable({
+        return(df_data())
         
     })
     
